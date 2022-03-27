3392891ebfd6200cb9de2c1252b11742d7e08430
Steven Smith <steven.76484@gmail.com>
2022-03-27 11:42:35 -0400

    add simple data layer

--- src/index.js
+++ src/index.js
@@ -3,4 +3,15 @@ import { fromEvent } from "rxjs";
 const $chagePageButton = document.getElementById("ChangePage");
 const changePage$ = fromEvent($chagePageButton, "click");
 
-changePage$.subscribe((_event) => console.log("change page now"));
+const dataLayer = (window.dataLayer = []);
+
+const trackPageView = () => {
+  dataLayer.push({
+    event: "page_view",
+    page: {
+      title: `New Page ${Date.now()}`,
+    },
+  });
+};
+
+changePage$.subscribe(trackPageView);
