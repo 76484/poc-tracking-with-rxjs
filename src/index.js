906f30af2d77abf19c9b9902bbc07b77de8189e4
Steven Smith <steven.76484@gmail.com>
2022-03-27 11:46:09 -0400

    add Add to Cart button

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
 import { fromEvent } from "rxjs";
 
+const $addToCart = document.getElementById("AddToCart");
 const $chagePageButton = document.getElementById("ChangePage");
+
+const addToCart$ = fromEvent($addToCart, "click");
 const changePage$ = fromEvent($chagePageButton, "click");
 
 const dataLayer = (window.dataLayer = []);
+
+const trackAddToCart = () => {
+  dataLayer.push({
+    event: "add_to_cart",
+    product: {
+      product_id: `${Math.round(Math.random() * 1000)}`,
+    },
+  });
+};
 
 const trackPageView = () => {
   dataLayer.push({
@@ -14,4 +26,5 @@ const trackPageView = () => {
   });
 };
 
+addToCart$.subscribe(trackAddToCart);
 changePage$.subscribe(trackPageView);
